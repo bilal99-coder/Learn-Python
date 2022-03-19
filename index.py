e97756e546272b27e29453386e91f2fed7d27f92
Bilal Sahli <Bilalsahli1999@gmail.com>
2022-03-19 23:39:11 +0100

    varibales and datatypes

--- index.py
+++ index.py
@@ -13,3 +13,13 @@ print(type('hello'))
 print(type(1))
 print(type(1.64))
 print(type(True))
+
+print('Variables & Datatypes - Exercise')
+#Create appropriate Variables for Item name, the price 
+#and how many you have in stock
+
+item_name = 'widget'
+price = 23.5
+inventory = 100
+is_in_inventory = True
+print(item_name, price, inventory)
