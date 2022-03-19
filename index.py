7539ec919ae38234cd000edc840f06ab20678673
Bilal Sahli <Bilalsahli1999@gmail.com>
2022-03-19 09:49:44 +0100

    the type function in python

--- index.py
+++ index.py
@@ -1,7 +1,15 @@
-failed_subjects=2. 
+failed_subjects= "2" 
 name='John'
 print('Dear Mrs Badger')
 print('Your son ' + name + ' is failing ' + failed_subjects + ' subjects.')
 print(name + '  will need to redo ' + failed_subjects + '  courses.')
 name="Eric"
 print(name + '  is doing well in geography.')
+
+# This how you get the type of a variable
+
+print(type(5))
+print(type('hello'))
+print(type(1))
+print(type(1.64))
+print(type(True))
