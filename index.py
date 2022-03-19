1dd6409834d82d1f656b34f512c93e42fde4ae4d
Bilal Sahli <Bilalsahli1999@gmail.com>
2022-03-19 23:53:50 +0100

    opeartions on strings

--- index.py
+++ index.py
@@ -23,3 +23,37 @@ price = 23.5
 inventory = 100
 is_in_inventory = True
 print(item_name, price, inventory)
+
+
+a=6
+b=2
+print('Addition : ', a + b)
+print('Subtraction : ', a - b)
+print('Multiplication : ', a * b)
+print('Division (float) : ', a / b)
+print('Division (floor) : ', a // b)
+print('Modulus : ', a % b)
+print('Exponent : ', a ** b)
+
+
+msg='welcome to it\'s Python 101: Strings'
+print(msg)
+print(msg.upper())
+print(msg.lower())
+print(msg.capitalize())
+print(msg.title())
+
+
+msg='welcome to Python 101: Strings'
+print(msg)
+print(len(msg))   #prints the length of the string 
+print(msg.count('Python')) #counts the instances of the word python 
+
+
+msg='welcome to Python 101: Strings'
+    #012345678
+print(msg)
+#slicing : get an element inside the string by its index 
+# if the index is negative, then python starts counting from the end of the # string 
+# if you use the index n: (i.e. msg[2:], python will grab all the elements # after 2)
+print(msg[0])
